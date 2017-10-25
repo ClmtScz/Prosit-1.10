ba49bf3c8704fe3cb71a332a145f68fa5c9124c4
Clément SANCHEZ <33086620+ClmtScz@users.noreply.github.com>
2017-10-25 17:36:48 +0200

    Squelette de touts les focntions dans le .c

--- rationnel.c
+++ rationnel.c
@@ -1,3 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float addition ( float a , float b) {
+
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+float soustraction ( float a , float b) {
+
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+float multiplication ( float a , float b) {
+
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+float division ( float a , float b) {
+
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+float comparaison ( float a , float b) {
+
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+float gcd ( float a , float b) {
+
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//

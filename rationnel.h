b8e4409e3bd08f69ee7cec24167c2a9853d0dbfd
Lapastoure Corentin <33086290+Lapastoure@users.noreply.github.com>
2017-10-25 16:23:25 +0200

    Update rationnel.h

--- rationnel.h
+++ rationnel.h
@@ -8,6 +8,7 @@ float addition (float a, float b);
 float soustraction (float a, float b);
 float multiplication (float a, float b);
 float division (float a, float b);
-
+float comparaison (float a, float b);
+float gcd (float a, float b);
 
 #endif // RATIONNEL_H_INCLUDED

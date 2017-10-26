3960c4dbe6f54f1d84039d871a25f1b1fd8a87d6
Thomas-Baudru <33087211+Thomas-Baudru@users.noreply.github.com>
2017-10-26 21:36:24 +0200

    Update rationnel.c

--- rationnel.c
+++ rationnel.c
@@ -6,31 +6,11 @@ float soustraction ( float n1 , float d1 , float n2 , float d2);
 float multiplication ( float n1 , float d1 , float n2 , float d2 );
 float division ( float n1 , float d1 , float n2 , float d2);
 float comparaison ( float n1 , float d1 , float n2 , float d2);
-float gcd ( float n1 , float d1, float n2, float d2);
+int PGCD(int n1, int n2);
 
-
-int T;
-
-do {
-
- if (a < b)
-
-{
-
- T = a;
-
- a = b;
-
- b = T;
-
-}
-
-a = a - b;
-
-} while (a);
-
-return b;
-  
+int PGCD(int n1, int n2) {
+  if (a % b == 0) return b;
+  return PGCD(b, a % b);
 }
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//

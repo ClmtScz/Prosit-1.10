90cd6b961a32b3a40f97d4f1efca01301bb49e79
Lapastoure Corentin <33086290+Lapastoure@users.noreply.github.com>
2017-10-26 18:26:22 +0200

    Create rationel.h

---
+++ rationel.h
@@ -0,0 +1,25 @@
+struct Fract
+  {
+   double numerateur;
+   double denominateur;
+  } Fract;
+
+   typedef struct Fract fraction;
+
+   double saisieFraction1 (fraction *fra1)
+   {
+        return (fra1->numerateur/fra1->denominateur);
+   }
+
+   double saisieFraction2 (fraction *fra2)
+   {
+     return (fra2->numerateur/fra2->denominateur);
+   }
+
+double additonFraction(fraction *fra1, fraction *fra2, fraction *i){
+
+i->numerateur = ((fra1->numerateur * fra2->denominateur) + ((fra2->numerateur) * (fra1->denominateur)));
+i->denominateur = i->numerateur / (fra1->denominateur * fra2->denominateur);
+
+ return 0;
+}

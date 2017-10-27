e5582c983243b6c4b91eb0d449c72ec97e36604e
Lapastoure Corentin <33086290+Lapastoure@users.noreply.github.com>
2017-10-27 10:13:57 +0200

    Update rationel.h

--- rationel.h
+++ rationel.h
@@ -16,10 +16,17 @@ struct Fract
      return (fra2->numerateur/fra2->denominateur);
    }
 
+
+
+
+
 double additonFraction(fraction *fra1, fraction *fra2, fraction *i){
 
 i->numerateur = ((fra1->numerateur * fra2->denominateur) + ((fra2->numerateur) * (fra1->denominateur)));
 i->denominateur = i->numerateur / (fra1->denominateur * fra2->denominateur);
 
  return 0;
+}
+double soustractionFraction(fraction *fra1, fraction *fra2, fraction *i){
+  
 }

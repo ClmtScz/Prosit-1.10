087d4e062c695f03026a3d236977bd72a1034f89
Tristan-Guerro <33086546+Tristan-Guerro@users.noreply.github.com>
2017-10-27 10:43:52 +0200

    Update rationnel.c

--- rationnel.c
+++ rationnel.c
@@ -12,8 +12,9 @@ double multiplication ( float n1 , float d1 , float n2 , float d2 );
 double division ( float n1 , float d1 , float n2 , float d2);
 double comparaison ( float n1 , float d1 , float n2 , float d2);
 double PGCD(int n1, int n2) {
-  if (a % b == 0) return b;
-  return PGCD(b, a % b);
-}
+  
+    if (a % b == 0) return b;
+  
+    return PGCD(b, a % b);
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+}

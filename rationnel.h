95656f081da88d0efc3df32c3c5a1e9b14fdf57d
Lapastoure Corentin <33086290+Lapastoure@users.noreply.github.com>
2017-10-27 11:32:57 +0200

    Update rationnel.h

--- rationnel.h
+++ rationnel.h
@@ -3,10 +3,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double soustraction (double n1 , double d1 , double n2 , double d2 );
-double multiplication (double n1 , double d1 , double n2 , double d2);
-double division (double n1 , double d1 , double n2 , double d2);
-double comparaison (double n1 , double d1 , double n2 , double d2);
-double gcd (double n1 , double d1 , double n2 , double d2);
+float addition ( float n1 , float d1 , float n2 , float d2){
+    float add;
+    add = ((n1*d2) + (n2*d1))/d1*d2;
+    return add;
+}
+
+float creatFraction(float n1 , float d1 , float n2 , float d2){
+     float Fract;
+     Fract = (n1/d1);
+     return Fract;
+}
+
+float soustraction ( float n1 , float d1 , float n2 , float d2 );
+float multiplication ( float n1 , float d1 , float n2 , float d2);
+float division ( float n1 , float d1 , float n2 , float d2);
+float comparaison ( float n1 , float d1 , float n2 , float d2);
+float gcd ( float n1 , float d1 , float n2 , float d2);
 
 #endif // RATIONNEL_H_INCLUDED

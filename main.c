710fed1d5605a7c148708ccefa73c30f28d43f22
Clément SANCHEZ <33086620+ClmtScz@users.noreply.github.com>
2017-10-27 09:28:15 +0200

    Update main.c

--- main.c
+++ main.c
@@ -8,24 +8,24 @@
 
 int main()
 {
-    float n1, d1, n2, d2;
+    double n1, d1, n2, d2;
     int choix;
     printf("------Bienvenue Que voulez vous faire--------\n");
     printf(".........1-Addition........\n........2-Soustraction.........\n");
-    scanf("%i",&choix);
+    scanf("%d",&choix);
 
     switch (choix) {
 
     case 1 : {
     printf("Numerateur 1 :");
-    scanf("%f",&n1);
+    scanf("%lf",&n1);
     printf("\nDenominateur 1 :");
-    scanf("%f",&d1);
+    scanf("%lf",&d1);
     printf("\nNumerateur 2 :");
-    scanf("%f",&n2);
+    scanf("%lf",&n2);
     printf("\nDenomiteur 2 :");
-    scanf("%f",&d2);
-    printf("\naddtion de %f / %f + %f / %f est : %f", n1, d1, n2, d2, addition(n1, d1, n2,d2));
+    scanf("%lf,&d2);
+    printf("\naddtion de %lf / %lf + %lf / %lf est : %lf", n1, d1, n2, d2, addition(n1, d1, n2,d2));
     break;
     }
 

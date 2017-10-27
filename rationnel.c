c39a25e8974b49ba6c0f9bda42378c4a38c55127
Clément SANCHEZ <33086620+ClmtScz@users.noreply.github.com>
2017-10-27 09:20:52 +0200

    Update rationnel.c

--- rationnel.c
+++ rationnel.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float addition ( float n1 , float d1 , float n2 , float d2 );
-float soustraction ( float n1 , float d1 , float n2 , float d2);
-float multiplication ( float n1 , float d1 , float n2 , float d2 );
-float division ( float n1 , float d1 , float n2 , float d2);
-float comparaison ( float n1 , float d1 , float n2 , float d2);
-int PGCD(int n1, int n2);
-
-int PGCD(int n1, int n2) {
+double addition ( float n1 , float d1 , float n2 , float d2 );
+double soustraction ( float n1 , float d1 , float n2 , float d2);
+double multiplication ( float n1 , float d1 , float n2 , float d2 );
+double division ( float n1 , float d1 , float n2 , float d2);
+double comparaison ( float n1 , float d1 , float n2 , float d2);
+double PGCD(int n1, int n2) {
   if (a % b == 0) return b;
   return PGCD(b, a % b);
 }

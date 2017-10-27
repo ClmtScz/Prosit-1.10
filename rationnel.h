493ab2b9180f096dbeaedfd1c183a98d6a974b6a
Thomas-Baudru <33087211+Thomas-Baudru@users.noreply.github.com>
2017-10-27 09:22:21 +0200

    Update rationnel.h

--- rationnel.h
+++ rationnel.h
@@ -1,17 +1,10 @@
 #ifndef RATIONNEL_H_INCLUDED
 #define RATIONNEL_H_INCLUDED
 
-float addition ( float n1 , float d1 , float n2 , float d2){
-    float add;
-    add = ((n1*d2) + (n2*d1))/d1*d2;
-    return add;
-}
-
-
-int soustraction (int n1 , int d1 , int n2 , int d2 );
-int multiplication (int n1 , int d1 , int n2 , int d2);
-int division (int n1 , int d1 , int n2 , int d2);
-int comparaison (int n1 , int d1 , int n2 , int d2);
-int gcd (int n1 , int d1 , int n2 , int d2);
+double soustraction (double n1 , double d1 , double n2 , double d2 );
+double multiplication (double n1 , double d1 , double n2 , double d2);
+double division (double n1 , double d1 , double n2 , double d2);
+double comparaison (double n1 , double d1 , double n2 , double d2);
+double gcd (double n1 , double d1 , double n2 , double d2);
 
 #endif // RATIONNEL_H_INCLUDED

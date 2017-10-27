8fbe9f28198523f745fcd0279f5eff1802d191fe
Tristan-Guerro <33086546+Tristan-Guerro@users.noreply.github.com>
2017-10-27 09:30:25 +0200

    Update mainv1.c

--- mainv1.c
+++ mainv1.c
@@ -1,34 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "ratio.h"
-   int main()
-{
+   
+int main() {
 
+    float n;
     fraction *fra1;
     fraction *fra2;
     fraction *i;
+    
     int choix;
-    printf("------Bienvenue Que voulez vous faire--------\n");
-    printf(".........1-Addition........\n........2-Soustraction.........\n");
+    printf("\t\t ---Bienvenu dans la calculatrice rationnel---"); 
+    printf("\n ---Que voulez vous faire---\n");
+    printf("\n\n0 = Quitter \n1 = Addition  \n2 = Soustraction \n3 = Multiplication \n4 = Division \n5 = Effacer\n");
     scanf("%i",&choix);
 
     switch (choix) {
 
-    case 1 : {
-    printf("\n---Numerateur :  ");
-    scanf("%lf", &fra1 *numerateur);
-    printf("\n----denominateur :");
-    scanf("%lf",&fra1->denominateur);
-    printf("\nLe resultat de la fraction est : %2f\n",saisieFraction1(fra1));
+    case 0 :
+       return 0;
     break;
-    }
-
-    case 2 : {
-
+       
+    case 1 :
+       printf("\n---Numerateur :  ");
+       scanf("%lf", &fra1 *numerateur);
+       printf("\n----denominateur :");
+       scanf("%lf",&fra1->denominateur);
+       printf("\nLe resultat de la fraction est : %2f\n",saisieFraction1(fra1));
+    break;
+          
+    case 2 : 
 
     break;
+          
+    case 3 : 
+
+    break;
+          
+    case 4 : 
+
+    break;
+          
+    case 5 : 
+      system("cls");
+    break;
+   
+      n++;
     }
-
-   }
-
+         
+         while(n != 0);
 }

462083ac832d78289ae74d4ffbf050853602596f
Clément SANCHEZ <33086620+ClmtScz@users.noreply.github.com>
2017-10-27 14:02:11 +0200

    Update rationnel.c

--- rationnel.c
+++ rationnel.c
@@ -1,11 +1,19 @@
-#include "operator.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "rationnel.h"
 
-typedef struct Fraction Fraction;
-struct Fraction {
-    long numerater[2];
-    long denominater[2];
-};
 
+long PGCD(long numerater, long denominater) {
+  if (numerater % denominater == 0) return denominater;
+  return PGCD(denominater, numerater % denominater);
+}
+//~~~~~~~~~~~~~//
+void simplification(long *numerateur, long *denominateur) {
+    long diviser = PGCD(*numerateur, *denominateur);
+    *numerateur /= diviser;
+    *denominateur /= diviser;
+}
+//~~~~~~~~~~~~~//
 void addition (long n1 , long d1 , long n2 , long d2){
     long denomCom, resultNum = 0;
     denomCom = d1 * d2;
@@ -13,7 +21,7 @@ void addition (long n1 , long d1 , long n2 , long d2){
     simplification(&resultNum, &denomCom);
     printf("\naddition de : %ld/%ld + %ld/%ld = %ld/%ld", n1, d1, n2, d2, resultNum, denomCom);
 }
-
+//~~~~~~~~~~~~~//
 void soustraction(long n1 , long d1 , long n2 , long d2){
     long denomCom, resultNum = 0;
     denomCom = d1 * d2;
@@ -21,47 +29,53 @@ void soustraction(long n1 , long d1 , long n2 , long d2){
     simplification(&resultNum, &denomCom);
     printf("\nsoustraction de : %ld/%ld - %ld/%ld = %ld/%ld", n1, d1, n2, d2, resultNum, denomCom);
 }
-
-long multiplication (long n1 , long d1 , long n2 , long d2 );
-long division (long n1 , long d1 , long n2 , long d2);
-long comparaison (long n1 , long d1 , long n2 , long d2);
-
-long PGCD(long numerater, long denominater) {
-  if (numerater % denominater == 0) return denominater;
-  return PGCD(denominater, numerater % denominater);
+//~~~~~~~~~~~~~//
+void multiplication (long n1 , long d1 , long n2 , long d2 ) {
+	long N, D;
+	N = n1 * n2;
+	D = d1 * d2;
+	simplification(&N, &D);
+	printf("  Le resultat est %ld / %ld", N, D);
 }
-
-void simplification(long *numerater, long *denominater) {
-    long diviser = PGCD(*numerater, *denominater);
-    *numerater /= diviser;
-    *denominater /= diviser;
+//~~~~~~~~~~~~~//
+void division (long n1 , long d1 , long n2 , long d2) {
+	long N, D;
+	N = n1 * d2;
+	D = n2 * d1;
+	simplification(&N, &D);
+	printf("  Le resultat est %ld / %ld", N, D);
 }
-int menu() {
-    Fraction fraction;
-    int choix;
-    printf("------Bienvenue Que voulez vous faire--------\n");
-    printf(".........1-Addition........\n........2-Soustraction.........\n........2-multiplication.........\n........2-division.........\n");
-    scanf("%d",&choix);
-    printf("Numerateur 1 : ");
-    scanf("%ld",&fraction.numerater[0]);
-    printf("\nDenominateur 1 : ");
-    scanf("%ld",&fraction.denominater[0]);
-    printf("\nNumerateur 2 : ");
-    scanf("%ld",&fraction.numerater[1]);
-    printf("\nDenomiteur 2 : ");
-    scanf("%ld",&fraction.denominater[1]);
-    printf("vos nombres rationnels : %ld/%ld    %ld/%ld", fraction.numerater[0], fraction.denominater[0], fraction.numerater[1], fraction.denominater[1]);
+//~~~~~~~~~~~~~//
+void comparaison (long n1 , long d1 , long n2 , long d2);
+//~~~~~~~~~~~~~//
 
+void menu() {
+	Fraction fraction;
+	int choix;
+	printf("------Bienvenue Que voulez vous faire--------\n");
+	printf(".........1-Addition........\n........2-Soustraction.........\n........3-multiplication.........\n........4-division.........\n");
+	scanf("%d",&choix);
+	printf("Numerateur 1 : ");
+	scanf("%ld",&fraction.numerater[0]);
+	printf("\nDenominateur 1 : ");
+	scanf("%ld",&fraction.denominater[0]);
+	printf("\nNumerateur 2 : ");
+	scanf("%ld",&fraction.numerater[1]);
+	printf("\nDenomiteur 2 : ");
+	scanf("%ld",&fraction.denominater[1]);
+	printf("vos nombres rationnels : %ld/%ld    %ld/%ld", fraction.numerater[0], fraction.denominater[0], fraction.numerater[1], fraction.denominater[1]);
     switch (choix) {
-
     case 1 :
-        addition(fraction.numerater[0], fraction.denominater[0], fraction.numerater[1], fraction.denominater[1]);
-        break;
-
-
-    case 2 : {
-        soustraction(fraction.numerater[0], fraction.denominater[0], fraction.numerater[1], fraction.denominater[1]);
-    break;
-    }
-   }
+			addition(fraction.numerater[0], fraction.denominater[0], fraction.numerater[1], fraction.denominater[1]);
+			break;
+    case 2 :
+			soustraction(fraction.numerater[0], fraction.denominater[0], fraction.numerater[1], fraction.denominater[1]);
+			break;
+		case 3 :
+			multiplication(fraction.numerater[0], fraction.denominater[0], fraction.numerater[1], fraction.denominater[1]);
+			break;
+		case 4 :
+			division(fraction.numerater[0], fraction.denominater[0], fraction.numerater[1], fraction.denominater[1]);
+			break;
+	}
 }

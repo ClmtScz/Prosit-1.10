8cc6d1899192ac489094a2345792e44952044f7d
Tristan-Guerro <33086546+Tristan-Guerro@users.noreply.github.com>
2017-10-27 10:45:59 +0200

    Update rationel.h

--- rationel.h
+++ rationel.h
@@ -16,17 +16,15 @@ struct Fract
      return (fra2->numerateur/fra2->denominateur);
    }
 
-
-
-
-
 double additonFraction(fraction *fra1, fraction *fra2, fraction *i){
 
-i->numerateur = ((fra1->numerateur * fra2->denominateur) + ((fra2->numerateur) * (fra1->denominateur)));
-i->denominateur = i->numerateur / (fra1->denominateur * fra2->denominateur);
+  i->numerateur = ((fra1->numerateur * fra2->denominateur) + ((fra2->numerateur) * (fra1->denominateur)));
+  i->denominateur = i->numerateur / (fra1->denominateur * fra2->denominateur);
 
  return 0;
+
 }
+
 double soustractionFraction(fraction *fra1, fraction *fra2, fraction *i){
   
 }
